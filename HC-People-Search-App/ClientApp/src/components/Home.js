d77474fb6fcd84f5b8d969ca39d646afe07b6635
Ryan <ryan@thecpafirm.com>
2021-05-17 20:39:53 -0600

    Load data to front end

--- HC-People-Search-App/ClientApp/src/components/Home.js
+++ HC-People-Search-App/ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SearchData } from './SearchData'
+import SearchData from './SearchData'
 
 export class Home extends Component {
   static displayName = Home.name;
--- HC-People-Search-App/ClientApp/src/components/SearchData.js
+++ HC-People-Search-App/ClientApp/src/components/SearchData.js
@@ -1,38 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-export class SearchData extends Component {
-  static displayName = SearchData.name;
+const useStyles = makeStyles((theme) => ({
+    searchField: {
+        width: '100%'
+    }
+}));
 
-// const SearchData = () => {
+// export class SearchData extends Component {
+//   static displayName = SearchData.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+const SearchData = () => {
+    const classes = useStyles()
+    // Setup of state to handle People Data Results
+    const [peopleData, setPeopleData] = useState({ dataRes: [null], loading: true })
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+    const populatePeopleData = async () => {
+        const response = await fetch('weatherforecast')
+        const data = await response.json()
+        setPeopleData({ dataRes: data, loading: false })
+    }
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+    useEffect(()=>{populatePeopleData()}, [])
+
+    const renderForecastsTable = (forecasts) => {
+        return (
+          <table className='table table-striped' aria-labelledby="tabelLabel">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Temp. (C)</th>
+                <th>Temp. (F)</th>
+                <th>Summary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {forecasts.map(forecast =>
+                <tr key={forecast.date}>
+                  <td>{forecast.date}</td>
+                  <td>{forecast.temperatureC}</td>
+                  <td>{forecast.temperatureF}</td>
+                  <td>{forecast.summary}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        );
+      }
 
 
-  render () {
-    
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+    console.log(peopleData)
+    let contents = peopleData.loading ? <p><em>Loading...</em></p> : renderForecastsTable(peopleData.dataRes);
 
     return (
         <div>
-            <TextField id='hcSearchField' label='Search' variant='outlined'></TextField>
+            <TextField className={classes.searchField} id='hcSearchField' label='Search' variant='outlined'></TextField>
+            {contents}
         </div>
     );
-  }
 }
+
+export default SearchData
